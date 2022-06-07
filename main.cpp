2162af5c0ee31c0dda8898f7981e7f3180341294
goldeneagle101 <school17mt@mail.ru>
2022-06-07 20:10:04 +0300

    изменение структуры ветки, т.к. в ходе разработки проекта была изменена структура

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include "LogiFirm_Server.h"
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    LogiFirmServer LogiFirm_server;  // создание сервера
-    return a.exec();
-}
